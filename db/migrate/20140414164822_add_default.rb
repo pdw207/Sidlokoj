e35210f231e5f22c96e0d6a2a06bd1c62dbb0b9d
Paul Wilson <pdw207@gmail.com>
2014-04-14 16:01:45 -0400

    Error in Migration Fixed that effects Production Deploy

--- db/migrate/20140414164822_add_default.rb
+++ db/migrate/20140414164822_add_default.rb
@@ -2,7 +2,7 @@ class AddDefault < ActiveRecord::Migration
   def up
     change_column :requests, :status, :string, null: false, default: 'Pending'
   end
-  def up
+  def down
     change_column :requests, :status, :string, null: false
   end
 end
