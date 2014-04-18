503c03c57915f11cfbec9e6b76dd678504f8e4e2
Paul Wilson <pdw207@gmail.com>
2014-04-18 14:00:16 -0400

    Styling File Input Button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,15 @@
 //= require foundation
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+function readURL(input) {
+if (input.files && input.files[0]) {
+var reader = new FileReader();
+
+reader.onload = function (e) {
+$('#img_prev')
+.attr('src', e.target.result);
+};
+
+reader.readAsDataURL(input.files[0]);
+}
+}
